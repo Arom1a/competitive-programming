df49714968c0b0f26c257495f5e689ca3cf83a08
Aromia <f2940015504@gmail.com>
2023-12-27 09:10:56 +0800

    fk restart

--- t.py
+++
@@ -1,7 +0,0 @@
-import random
-
-INT_MAX = 2**31-1
-
-with open('./bin/input.in','w') as f:
-    for i in range(10000):
-        f.write(str(random.randint(0,INT_MAX)) + ' ')
--- test.cpp
+++ test.cpp
@@ -1,58 +1,74 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-vector<int> split(string ori_str)
+void print_vector(vector<pair<int, int>> my_vec)
 {
-    int end = ori_str.find(' ');
-    int length = count(ori_str.begin(), ori_str.end(), ' ') + 1;
-    vector<int> return_array(length);
-    for (int i = 0; i < length; i++)
+    for (auto i : my_vec)
     {
-        int piece = stoi(ori_str.substr(0, end));
-        return_array[i] = piece;
-        ori_str = ori_str.substr(end + 1);
-        end = ori_str.find(' ');
+        cout << "| " << i.first << ' ' << i.second << ' ';
     }
-    return return_array;
+    cout << "|\n";
 }
-
-// int main(int argc, char** argv) {
-
-//     std::string line;
-//     int number;
-//     std::vector<int> numbers;
-
-//     std::cout << "Enter numbers separated by spaces: ";
-//     std::getline(std::cin, line);
-//     std::istringstream stream(line);
-//     while (stream >> number)
-//         numbers.push_back(number);
-
-//     write_vector(numbers);
-
-// }
 
 int main()
 {
-    int initial_number;
-    string hand_number_str;
-    string tile_number_str;
+    // string line1, line2, line3, tmp_str;
+    // getline(cin, line1);
+    // getline(cin, line2);
+    // getline(cin, line3);
 
-    // cin >> initial_number;
-    // cin.ignore(10, '\n');
-    // getline(cin, hand_number_str);
-    // getline(cin, tile_number_str);
+    string line1 = "5923", line2 = "56 27 73 34 99 45 32 17 64 57 18 11", line3 = "36 92 22 50 82", tmp_str;
+    // expected: 16
 
-    initial_number = 5923;
-    hand_number_str = "27 73 34 99 45 32 17 64 57 18 11";
-    tile_number_str = "92 22 50 82";
+    stringstream ss1(line1), ss2(line2), ss3(line3);
+    string row_str;
+    vector<pair<int, int>> hand, pile;
+    while (ss1 >> tmp_str)
+    {
+        while (tmp_str.length() < 4)
+        {
+            tmp_str = '0' + tmp_str;
+        }
+        row_str = tmp_str;
+    }
+    vector<int> rows{stoi(row_str[0]), stoi(row_str[1]), stoi(row_str[2]), stoi(row_str[3])};
+    while (ss2 >> tmp_str)
+    {
+        if (tmp_str.length() == 1)
+            tmp_str = '0' + tmp_str;
+        hand.push_back({stoi(tmp_str[0]), stoi(tmp_str[1])});
+    }
 
-    int number_of_space;
-    number_of_space = count(hand_number_str.begin(), hand_number_str.end(), ' ') + 1;
-    vector<int> hand_number = split(hand_number_str);
-    number_of_space = count(tile_number_str.begin(), tile_number_str.end(), ' ') + 1;
-    vector<int> tile_number = split(tile_number_str);
+    while (ss3 >> tmp_str)
+    {
+        if (tmp_str.length() == 1)
+            tmp_str = '0' + tmp_str;
+        pile.push_back({stoi(tmp_str[0]), stoi(tmp_str[1])});
+    }
+
+    for (auto i : hand)
+    {
+        for (auto &row : rows)
+        {
+            if (i.first == row)
+            {
+                row = i.second;
+                if (i.first == i.second)
+                {
+
+                }
+            }
+            else if (i.second == row)
+            {
+                row = i.first;
+                break;
+            }
+            else
+            {
+                hand.push_back(pile[0]);
+                pile.erase(pile.begin());
+            }
+        }
+    }
 }
