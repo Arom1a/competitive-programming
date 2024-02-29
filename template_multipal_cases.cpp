8878726fb13fafcc939c225ae2f877308d956a74
Aromia <f2940015504@gmail.com>
2024-02-29 16:04:30 +0800

    feat: add template for multipal-case questions

---
+++ template_multipal_cases.cpp
@@ -0,0 +1,38 @@
+#include "bits/stdc++.h"
+
+typedef long long ll;
+using namespace std;
+
+// void print_vector(const vector<> &my_vec)
+// {
+//     for (auto &i : my_vec)
+//     {
+//         cout << i << ' ';
+//     }
+//     cout << '\n';
+// }
+
+// // test
+// int N = ;
+// vector<int> c{};
+// // expected:
+
+int N, Q;
+int main()
+{
+    // input
+    cin >> N >> Q;
+
+    while (Q--)
+    {
+        vector<int> c(N);
+        for (int i = 0; i < N; i++)
+        {
+        }
+
+        // answer below
+        int ans = 0;
+
+        cout << ans << '\n';
+    }
+}
