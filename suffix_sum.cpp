ae5a84b0926d8dd3519c31ec950c8257412d6c3b
Aromia <f2940015504@gmail.com>
2024-03-04 08:14:29 +0800

    implement suffix sum algorithm

---
+++ suffix_sum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <numeric>
+
+template <typename T>
+void print_vector(const std::vector<T> &my_vec)
+{
+    for (const T &i : my_vec)
+        std::cout << i << ' ';
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::vector<int> sample{1, 2, 3, 4, 5};
+    std::vector<int> sum(5);
+    std::partial_sum(sample.rbegin(), sample.rend(), sum.rbegin());
+    print_vector(sum);
+    // sum [0,3): 1+2+3 = 6
+    // sum [0,2]
+    std::cout << sum[0] - sum[3] << '\n';
+    // sum [1,4): 2+3+4 = 9
+    // sum [1,3]
+    std::cout << sum[1] - sum[4] << '\n';
+}
