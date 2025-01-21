fd70d1e06a350146493bf38a462df2d13a6feec1
Aromia <f2940015504@gmail.com>
2025-01-21 21:30:57 +0800

    cses/concert_tickets done

---
+++ cses/concert_tickets.cpp
@@ -0,0 +1,38 @@
+// https://cses.fi/problemset/task/1091
+// #multiset
+// lession: learn how to use multiset
+
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+
+    multiset<int> t;
+
+    for (int i = 0; i < n; i++) {
+        int h_i;
+        cin >> h_i;
+        t.insert(h_i);
+    }
+
+    for (int i = 0; i < m; i++) {
+        int t_i;
+        cin >> t_i;
+        auto it = t.upper_bound(t_i);
+        if (it == t.begin()) {
+            cout << -1 << '\n';
+            continue;
+        }
+
+        it = prev(it);
+
+        cout << *it << '\n';
+        t.erase(it);
+    }
+}
