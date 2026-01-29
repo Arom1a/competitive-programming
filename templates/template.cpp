b13f1ada2ab67d9bb93f2304601d96cd842412a6
Aromia <f2940015504@gmail.com>
2026-01-28 23:59:34 -0500

    chore: update template

--- templates/template.cpp
+++ templates/template.cpp
@@ -5,15 +5,22 @@ using namespace std;
 #define fio                  \
     ios::sync_with_stdio(0); \
     cin.tie(NULL)
+template <typename T>
+using vec = vector<T>;
 typedef long long ll;
-typedef vector<int> vi;
-typedef vector<long long> vll;
+typedef vec<int> vi;
+typedef vec<vec<int>> vvi;
+typedef vec<ll> vll;
+typedef vec<vec<ll>> vvll;
+typedef vec<bool> vb;
+typedef vec<vec<bool>> vvb;
+typedef vec<string> vs;
+typedef vec<vec<string>> vvs;
 
 void solve() {}
 
 int main() {
     fio;
-
     int T;
     cin >> T;
     while (T--) {
